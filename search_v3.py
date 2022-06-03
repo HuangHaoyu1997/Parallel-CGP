ba6d12524a19e0b177d69c68665d8e2c584a8e3d
Haoyu Huang <31029753+HuangHaoyu1997@users.noreply.github.com>
2022-06-03 16:10:32 +0800

    1

--- search_v3.py
+++ search_v3.py
@@ -1,31 +1,28 @@
-import pickle, cma, copy, gym
+import pickle, cma, copy, gym, ray, time
 from cgp import *
+from configuration import config
 
-with open('./results/CGP_SP-29.pkl', 'rb') as f:
-    pop:Individual = pickle.load(f)
+# ray.init(num_cpus=config.n_process)
 
-print(pop[0].fitness)
+with open('./results/CGP_SP-79.pkl', 'rb') as f:
+    pop = pickle.load(f)
+
+
 pop[0]._determine_active_nodes()
 weights, n_active = [], 0
 for node in pop[0].nodes:
     if node.active:
         n_active += 1
-        print(node.weights)
         weights.extend(node.weights)
-print(weights, n_active)
-es = cma.CMAEvolutionStrategy(
-    x0=[1.0]*len(weights),
-    sigma0=1.0,
-    inopts={'popsize':7},
-)
-solution = es.ask()
-print(solution[0])
+
 individual:Individual = copy.deepcopy(pop[0])
+es = cma.CMAEvolutionStrategy(
+    x0=[-1.]*len(weights),
+    sigma0=1.0,
+    inopts={'popsize':20},
+)
 
-
-
-# print([node.weights for node in individual.nodes])
-
+@ray.remote
 def func(ind:Individual, solution):
     ind._determine_active_nodes()
     pointer = 0
@@ -33,12 +30,25 @@ def func(ind:Individual, solution):
         if node.active:
             node.weights = solution[pointer:pointer+node.arity]
             pointer += node.arity
-    
-    env = gym.make('Pendulum-v1')
-    s = env.reset()
-    done, rr = False, 0
-    while not done:
-        action = ind.eval(*s)
-        s, r, done, _ = env.step(action)
-        rr += r
-    return rr
+    reward = 0
+    for i in range(config.Epoch):
+        env = gym.make('Pendulum-v1')
+        env.seed(int(time.time()*1000))
+        s = env.reset()
+        done, rr = False, 0
+        while not done:
+            action = ind.eval(*s)
+            s, r, done, _ = env.step([action])
+            rr += r
+            reward += r
+    return -reward/config.Epoch
+print('best fitness before weight search:', pop[0].fitness)
+print('start training')
+for i in range(config.N_GEN):
+    solutions = es.ask()
+    fit = [func.remote(individual, solution) for solution in solutions]
+    fitness = ray.get(fit)
+    es.tell(solutions, fitness)
+    best_f, best_x = -es.best.f, es.best.x
+    print(i, best_f)
+
